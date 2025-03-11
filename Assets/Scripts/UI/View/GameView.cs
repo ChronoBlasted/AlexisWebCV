aca49b2096c64a7f2f8ef712a05493acba267777
ChronoBlasted <74500014+ChronoBlasted@users.noreply.github.com>
2025-03-11 22:05:51 +0100

    Init

---
+++ Assets/Scripts/UI/View/GameView.cs
@@ -0,0 +1,18 @@
+public class GameView : View
+{
+    public override void Init()
+    {
+        base.Init();
+    }
+
+    public override void OpenView(bool _instant = false, float timeToOpen = 0.2F)
+    {
+        base.OpenView(_instant, timeToOpen);
+    }
+
+    public override void CloseView()
+    {
+        base.CloseView();
+    }
+
+}
